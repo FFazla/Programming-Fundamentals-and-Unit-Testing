b029c6087e3ee258b2ece74b416be75a0924f22c
Fazla Fazla <76998662+FFazla@users.noreply.github.com>
2023-10-21 19:46:54 +0200

    Add files via upload

---
+++ Simple Conditional Statements/2.EvenOrOdd/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _2.EvenOrOdd;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        if( n % 2 == 0)
+        {
+            Console.WriteLine("even");
+        }
+        else
+        {
+            Console.WriteLine("odd");
+        }
+    }
+}
+
