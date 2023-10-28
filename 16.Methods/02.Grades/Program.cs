f551e10d81fcf7e8e5f59f708063ea2c8e53edf3
Fazla Fazla <f.fazla.f@gmail.com>
2023-10-28 18:47:13 +0200

    Update

---
+++ 16.Methods/02.Grades/Program.cs
@@ -0,0 +1,28 @@
+﻿static void GradeInWord (double grade)
+{
+    if (grade >= 2 && grade < 3)
+    {
+        Console.WriteLine("Fail");
+    }
+    else if (grade >= 3 && grade < 3.5)
+    {
+        Console.WriteLine("Average");
+    }
+    else if (grade >= 3.5 && grade < 4.5)
+    {
+        Console.WriteLine("Good");
+    }
+    else if (grade >= 4.5 && grade < 5.5)
+    {
+        Console.WriteLine("Very good");
+    }
+    else if (grade >= 5.5 && grade <= 6)
+    {
+        Console.WriteLine("Excellent");
+    }
+
+
+}
+double grade = double.Parse(Console.ReadLine());
+
+GradeInWord(grade);
