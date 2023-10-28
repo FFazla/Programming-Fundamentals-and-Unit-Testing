6a03766aacc1fd32d5564d9b69fa9b05dd7073a4
Fazla Fazla <f.fazla.f@gmail.com>
2023-10-28 20:21:22 +0200

    Update

---
+++ 18.Arrays/05.EqualArrays/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] arr1 = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int[] arr2 = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+bool isIdentical = true;
+
+for(int index = 0; index <= arr1.Length - 1; index++)
+{
+    if (arr1[index] != arr2[index])
+    {
+        isIdentical = false;
+        Console.WriteLine("Arrays are not identical.");
+        break;
+    }
+    
+}
+if (isIdentical)
+{
+    Console.WriteLine("Arrays are identical.");
+}
---
+++ 18.Arrays/06.CommonElements/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] arr1 = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int[] arr2 = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+var commonElements = arr1.Intersect(arr2);
+
+if (commonElements.Any())
+{
+    Console.WriteLine(string.Join(" ", commonElements));
+}
+else
+{
+    Console.WriteLine("null");
+}
